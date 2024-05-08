d7abd2bdb143ae3e1b85615ef6acbed8431deb0c
kapilnagar72 <kapilnagar14820@gmail.com>
2024-05-09 00:23:08 +0530

    BubbleSort

---
+++ BubbleSort.java
@@ -0,0 +1,30 @@
+package Searching_Sorting_and_BitManipulation;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void BubbleSort(int [] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            boolean swapped = false;
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+
+                    swapped = true;
+                }
+            }
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 20, 50, 10, 12, 45, 67, 90 };
+        BubbleSort(arr);
+        System.out.println("Sorted array is:");
+        System.out.println(Arrays.toString(arr));
+    }
+}
